cc7b14cdfc5169c1b88e16a481c328725979027a
Tien Ngo <ngoquoctien24196@gmail.com>
2022-08-14 01:14:40 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3003;
+const fs = require("fs");
+const path = require("path");
+const router = require("express").Router();
+
+
