f6181d5833aac7468f5b256f8262ee6fc64f4932
Tien Ngo <ngoquoctien24196@gmail.com>
2022-08-15 03:09:05 -0400

    add funtion and route to delete selected note

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
+//require express and needed files
 const express = require('express');
-const {notes} = require('./db/db');
+const notes = require('./db/db');
 const app = express();
-// parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 const PORT = process.env.PORT || 3003;
 const fs = require("fs");
 const path = require("path");
-const router = require("express").Router();
 
+// function to add new note to db.json
 function createNewNote(body, notesArray) {
     const note = body;
     notesArray.push(note);
@@ -18,11 +18,10 @@ function createNewNote(body, notesArray) {
         path.join(__dirname, './db/db.json'),
         JSON.stringify({ notes: notesArray }, null, 2)
       );
-
     return note;
-
   };
 
+// validate new note
 function validateNote(note) {
     if (!note.title) {
       return false;
@@ -33,28 +32,44 @@ function validateNote(note) {
     return true;
   } ;
 
+// function to delete selected note and rewrite notes
+function deleteNote(id, notesArray) {
+    for (let i = 0; i < notesArray.length; i++) {
+        let note = notesArray[i];
 
-app.use(express.static('public'));
+        if (note.id === id) {
+            notesArray.splice(i, 1);
+            fs.writeFileSync(
+                path.join(__dirname, './db/db.json'),
+                JSON.stringify(notesArray, null, 2)
+            );
+
+            break;
+        }
+    }
+  }
+
+
+// generate index.html when main route is called
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+// generate notes.html when /notes route is called
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'))
 });
 
-
+// route to get notes from db.json
 app.get('/api/notes', (req, res) => {
     res.json(notes);
 })
 
 
-
+//accept new note from post request
 app.post('/api/notes', (req, res) => {
 
     req.body.id = notes.length.toString();
-
-
     if (!validateNote(req.body)) {
         res.status(400).send('The note is not properly formatted.');
       } else {
@@ -65,13 +80,15 @@ app.post('/api/notes', (req, res) => {
     
 });
 
-
-
-
+// delate selected note
+app.delete('/api/notes/:id', (req, res) => {
+  deleteNote(req.params.id, notes);
+  res.json(true);
+});
 
 
 
 app.listen(PORT, () => {
-    console.log("API server now on port 3003!");
+    console.log(`API server now on port ${PORT}!`);
    
 })
